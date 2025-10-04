255853215c1a05f6bd59a5ee8dcf8c923db8da01
OrangeMonster <91896426+omthegg@users.noreply.github.com>
2025-10-04 18:33:48 +0330

    Procedural fire progress

--- source/components/environment_controller/environment_controller.gd
+++ source/components/environment_controller/environment_controller.gd
@@ -17,5 +17,5 @@ func _process(delta: float) -> void:
 
 func animate_fire_flicker() -> void:
 	world_environment.environment.ambient_light_color = Color.ORANGE
-	var light_energy:float = 0.1 + abs(fire_noise.get_noise_1d(time * 30))/10
+	var light_energy:float = 0.05 + abs(fire_noise.get_noise_1d(time * 30))/10
 	world_environment.environment.ambient_light_energy = light_energy
--- source/fire/fire.gd
+++ source/fire/fire.gd
@@ -8,10 +8,18 @@ func _ready() -> void:
 	$AnimatedSprite3D2.play("fire")
 	$AnimatedSprite3D3.play("fire")
 	$AnimatedSprite3D4.play("fire")
+	#$Sprite3D.modulate = Color.ORANGE
 
 
 func _physics_process(delta: float) -> void:
 	time += delta
 	
-	noise.seed = int(time * 10)
-	$Sprite3D.texture = ImageTexture.create_from_image(noise.get_image(64, 64))
+	$Sprite3D.texture.noise.seed = int(time * 10)
+	
+	var looking_position:Vector3 = get_tree().root.get_camera_3d().global_position
+	looking_position.y = $MeshInstance3D.global_position.y
+	#$MeshInstance3D.look_at(looking_position)
+	
+	var shader_noise:FastNoiseLite = $MeshInstance3D.material_override.get_shader_parameter("noise_texture").noise
+	#shader_noise.seed = int(time * 10)
+	shader_noise.offset.y += 100.0 * delta
