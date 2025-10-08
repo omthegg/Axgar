296b099036a57c0e9c7bd99a3045a5744bbfe5c7
OrangeMonster <91896426+omthegg@users.noreply.github.com>
2025-10-08 22:12:41 +0330

    Fire progress

--- source/scene_manager/scene_manager.gd
+++ source/scene_manager/scene_manager.gd
@@ -5,6 +5,27 @@ extends Node3D
 @onready var draw_calls_label:Label = $DebugCanvasLayer/Control/DrawCallsLabel
 @onready var primitives_label:Label = $DebugCanvasLayer/Control/PrimitivesLabel
 
+@onready var console_canvas_layer:CanvasLayer = $ConsoleCanvasLayer
+@onready var console_line_edit:LineEdit = $ConsoleCanvasLayer/Control/LineEdit
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		if console_canvas_layer.visible:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			console_canvas_layer.hide()
+			return
+		
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+	if event.is_action_pressed("console"):
+		console_canvas_layer.visible = !console_canvas_layer.visible
+		console_line_edit.grab_focus()
+		console_line_edit.text = ""
+
 
 func _process(_delta: float) -> void:
 	fps_label.text = "FPS: " + str(int(Engine.get_frames_per_second()))
